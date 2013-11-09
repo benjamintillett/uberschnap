b3be93fb08e0dcfba29c89a9c2c03bb027912c59
ben tillett <benjamin.tillett@gmail.com>
2013-11-09 17:42:02 +0000

    add     config.assets.initialize_on_precompile = false to application.rb to allow push to heroku

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,7 @@ module Uberschnap
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+        # FIX to allow push to heroku
+    config.assets.initialize_on_precompile = false
   end
 end
