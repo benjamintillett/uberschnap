26c4b8c8035fda73c78aefb252b261744a7d1b8e
ben tillett <benjamin.tillett@gmail.com>
2013-11-09 17:30:19 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uberschnap::Application.config.secret_token = '55fb3f65f1776dafd36ec9ed852231a693b456fe720181f65d2624a7b4ec93b84bd57303f70bc61380156b1900929992acb4c6de1307f3256b8ccf18ad01b989'
