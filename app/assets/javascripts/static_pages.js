bbb51ed92e7bf0daaae0d76b1915e230be4e0b77
ben tillett <benjamin.tillett@gmail.com>
2013-11-23 12:01:47 +0000

    add all photobooth images and make changes requested by jasper

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -10,5 +10,10 @@ $(document).ready(function(){
 		  interval: 5000
 		});
     }, 2500);
-
+    
+    $(".nav-list > li > a, #side-bar-title > a").click(function(){
+  		$(".nav-list > li > a").css("color","#B24EC6");
+  		$("#side-bar-title > a").css("color","#B24EC6");
+  		$(this).css("color","black");
+		});
 });
